cf17757ed927c50fb4a64b0b6f02452dfeef373a
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-06 16:05:01 +0300

    Add get random element

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
 const ObjectId = require('mongodb').ObjectId;
 
 // endpoints
@@ -16,7 +15,15 @@ router.get('/departments', (req, res) => {
 });
 
 router.get('/departments/random', (req, res) => {
-  res.json(db.departments[Math.floor(Math.random() * db.length)]);
+  //res.json(db.departments[Math.floor(Math.random() * db.length)]);
+
+  req.db
+    .collection('departments')
+    .aggregate([{ $sample: { size: 3 } }])
+    .toArray((err, data) => {
+      if (err) res.status(500).json({ message: err });
+      else res.json(data[0]);
+    });
 });
 
 router.get('/departments/:id', (req, res) => {
@@ -33,7 +40,7 @@ router.get('/departments/:id', (req, res) => {
 
 router.post('/departments', (req, res) => {
   const { name } = req.body;
-
+  // db.departments.push({ id: 3, name })
   req.db.collection('departments').insertOne({ name: name }, (err) => {
     if (err) res.status(500).json({ message: err });
     else res.json({ message: 'OK' });
@@ -42,6 +49,7 @@ router.post('/departments', (req, res) => {
 
 router.put('/departments/:id', (req, res) => {
   const { name } = req.body;
+  // db = db.departments.map(item => (item.id == req.params.id) ? { ...item, name } : item );
   req.db
     .collection('departments')
     .updateOne(
@@ -55,6 +63,7 @@ router.put('/departments/:id', (req, res) => {
 });
 
 router.delete('/departments/:id', (req, res) => {
+  // db = db.departments.filter(item => item.id != req.params.id)
   req.db
     .collection('departments')
     .deleteOne({ _id: ObjectId(req.params.id) }, (err) => {
