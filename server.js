60e6a56f105811a11c9e1fd73d7b18c684c83185
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-08-06 13:32:04 +0300

    init files with comments

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const mongoClient = require('mongodb').MongoClient;
+
+const employeesRoutes = require('./routes/employees.routes');
+const departmentsRoutes = require('./routes/departments.routes');
+const productsRoutes = require('./routes/products.routes');
+
+// server.js not only connects to the database, but also allows you to access it from the Express server via the db object
+mongoClient.connect(
+  'mongodb://localhost:27017',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Successfully connect to the database');
+
+      const db = client.db('companyDB');
+      const app = express();
+
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: false }));
+
+      // middleware
+      app.use((req, res, next) => {
+        req.db = db;
+        next();
+      });
+      app.use('/api', employeesRoutes);
+      app.use('/api', departmentsRoutes);
+      app.use('/api', productsRoutes);
+
+      app.use((req, res) => {
+        res.status(404).send({ message: 'Not found...' });
+      });
+
+      app.listen('8000', () => {
+        console.log('Server is running on port: 8000');
+      });
+    }
+  }
+);
